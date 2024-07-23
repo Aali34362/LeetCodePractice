953b3852c17de258a37eb6221b6b884ec46430c6
Aamir Ali <aa882755@gmail.com>
2024-07-23 08:25:19 +0530

    Two Sums

---
+++ LeetCodePractice/Easy/TwoSum.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCodePractice.Easy;
+
+public class TwoSum
+{
+    //My Thinking
+    public int[] TwoSumFunction(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int currentIndex = i;
+            int nextIndexofI = i + 1;
+            for (int j = nextIndexofI; j < nums.Length; j++)
+            {
+                if (nums[currentIndex] + nums[j] == target)
+                    return [currentIndex, j];
+            }
+        }
+        return [0, 0];
+    }
+
+    //Chat GPT
+
+    //Leet Code Solutions 
+}
+
+
+/*
+ * 1. Two Sum
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order. 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+ */
--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -1,5 +1,9 @@
 ﻿Console.WriteLine("Leet Code Practice Started On 23-07-2024");
 
-SortArrayByIncreasingFrequency sortArrayByIncreasingFrequency = new();
-sortArrayByIncreasingFrequency.FrequencySort([1, 1, 2, 2, 2, 3]).Dump();
-sortArrayByIncreasingFrequency.FrequencySort([2, 3, 1, 3, 2]).Dump();
+////SortArrayByIncreasingFrequency sortArrayByIncreasingFrequency = new();
+////sortArrayByIncreasingFrequency.FrequencySort([1, 1, 2, 2, 2, 3]).Dump();
+////sortArrayByIncreasingFrequency.FrequencySort([2, 3, 1, 3, 2]).Dump();
+
+TwoSum twoSum = new TwoSum();
+twoSum.TwoSumFunction([2, 7, 11, 15], 9).Dump();
+twoSum.TwoSumFunction([3, 2, 3], 6).Dump();
