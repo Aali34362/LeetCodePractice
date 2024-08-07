ea48c01af0da2b3787527ca68d42bb0a5ba8dfb4
Aamir Ali <aa882755@gmail.com>
2024-08-07 08:26:15 +0530

    MergeTwoSortedLists

---
+++ LeetCodePractice/Easy/MergeTwoSortedLists.cs
@@ -0,0 +1,58 @@
+﻿namespace LeetCodePractice.Easy;
+
+public static class MergeTwoSortedLists
+{
+    public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        Console.WriteLine($"ListNode 1  : {list1}");
+        Console.WriteLine($"ListNode 2  : {list2}");
+        ListNode head = new();
+        Console.WriteLine($"Head : {head}");
+        ListNode tail = head;
+        Console.WriteLine($"Tail : {tail}");
+
+        while (list1 is not null && list2 is not null)
+        {
+            Console.WriteLine($"list1 value : {list1.val} and list2 value : {list2.val}");
+            if(list1.val < list2.val)
+            {
+                tail.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            Console.WriteLine($"list1 : {list1}");
+            Console.WriteLine($"list2 : {list2}");
+            Console.WriteLine($"Tail Next : {tail}");
+            tail = tail.next;
+        }
+
+        if(list1 is not null)
+        {
+            tail.next = list1;
+        }
+        if (list2 is not null)
+        {
+            tail.next = list2;
+        }
+        Console.WriteLine($"Tail End : {tail}");
+        Console.WriteLine($"Head End : {head}");
+        return head.next;
+    }
+}
+
+
+//Definition for singly-linked list.
+public record ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -23,7 +23,21 @@ Console.WriteLine("Leet Code Practice Started On 23-07-2024");
 
 ///LongestCommonPrefix.LongestCommonPrefixFunc2(["interspecies", "interstellar", "interstate"]).Dump();
 ///
-ValidParantheses.IsValid("(([]){})").Dump();
+////ValidParantheses.IsValid("(([]){})").Dump();
+
+ListNode listNode6 = new ListNode() { val = 4 };
+ListNode listNode5 = new ListNode() { val = 3, next = listNode6 };
+ListNode listNode4 = new ListNode() { val = 1, next = listNode5 };
+
+ListNode listNode3 = new ListNode() { val = 4 }; 
+ListNode listNode2 = new ListNode() { val = 2, next = listNode3 }; 
+ListNode listNode1 = new ListNode() { val = 1, next = listNode2 };
+
+ListNode listNode7 = new ListNode(1, new ListNode(2, new ListNode(4)));
+ListNode listNode8 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+MergeTwoSortedLists.MergeTwoLists(listNode1, listNode4).Dump();
+MergeTwoSortedLists.MergeTwoLists(listNode7, listNode8).Dump();
 
 
 
